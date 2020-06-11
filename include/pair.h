4c0cd8b4374b72da6d396a341e4ce840ddb340c0
myyrakle <sssang97@naver.com>
2020-06-11 13:17:05 +0900

    Update pair.h

--- include/pair.h
+++ include/pair.h
@@ -2,14 +2,24 @@
 #define __PRETTY_LOG_PAIR__
 
 #include <ostream>
-
+#include <sstream>
+#include <string>
 #include <utility>
+
+template <class T1, class T2>
+std::string to_string(const std::pair<T1, T2>& p)
+{
+  std::ostringstream sout;
+  
+  sout<<"pair ("<<p.first<<", "<<p.second<<")";
+  
+  return sout.str();
+}
 
 template <class T1, class T2>
 std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p)
 {
-  std::cout<<"pair ("<<p.first<<", "
-    <<p.second<<")";
+  os<<to_string(p);
   
   return os;
 }
