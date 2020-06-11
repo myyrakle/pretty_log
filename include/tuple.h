d1d2ff67b53fb51739584e27f1ad043dbf9c78c7
myyrakle <sssang97@naver.com>
2020-06-11 13:27:06 +0900

    Update tuple.h

--- include/tuple.h
+++ include/tuple.h
@@ -3,34 +3,46 @@
 
 #include <ostream>
 
+#include <sstream>
+#include <string>
+
 #include <tuple>
 #include <type_traits>
 
 template<std::size_t I = 0, class... T>
 static typename std::enable_if<I == sizeof...(T), void>::type
-  __print_tuples(const std::tuple<T...>& t)
+  __print_tuples(const std::tuple<T...>& t, std::ostringstream& sout))
 { }
 
 template<std::size_t I = 0, class... T>
 static typename std::enable_if<I < sizeof...(T), void>::type
-  __print_tuples(const std::tuple<T...>& t)
+  __print_tuples(const std::tuple<T...>& t, std::ostringstream& sout)
 {
   if(I!=0)
   {
-    std::cout<<", ";
+    sout<<", ";
   }
     
-  std::cout << std::get<I>(t);
-  __print_tuples<I+1, T...>(t);
+  sout << std::get<I>(t, sout);
+  __print_tuples<I+1, T...>(t, sout);
 }
 
+template <class ...T>
+std::string to_string(const std::tuple<T...>& t)
+{
+  std::ostringstream sout;
+
+  sout<<"tuple (";
+  __print_tuples(t, sout);
+  sout<<")";
+  
+  return sout.str();
+}
 
 template <class ...T>
 std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t)
 {
-  std::cout<<"tuple (";
-  __print_tuples(t);
-  std::cout<<")";
+  os<<to_string(t);
   
   return os;
 }
