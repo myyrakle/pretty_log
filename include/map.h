610e7c6fc11d67143023291b767a9d71de42a691
myyrakle <sssang97@naver.com>
2020-06-12 05:56:16 +0900

    Create map.h

---
+++ include/map.h
@@ -0,0 +1,83 @@
+#ifndef __PRETTY_LOG_MAP__
+#define __PRETTY_LOG_MAP__
+
+#include <map>
+#include <sstream>
+#include <string>
+
+template <class K, class V>
+std::string to_string(const std::map<K, V>& m)
+{
+  std::ostringstream sout;
+  
+  sout<<"map { ";
+  
+  auto it = m.begin();
+  
+  if(it!=m.end())
+  {
+    sout<<it->first<<":"<<it->second;
+    it++;
+  }
+  
+  while(it!=m.end())
+  {
+    sout<<", "<<it->first<<":"<<it->second;
+    it++;
+  }
+  
+  sout<<" }";
+  
+  return sout.str();
+}
+
+#include <ostream>
+
+template <class K, class V>
+std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
+{
+  os<<to_string(m);
+  
+  return os;
+}
+
+
+
+template <class K, class V>
+std::string to_string(const std::multimap<K, V>& m)
+{
+  std::ostringstream sout;
+  
+  sout<<"multimap { ";
+  
+  auto it = m.begin();
+  
+  if(it!=m.end())
+  {
+    sout<<it->first<<":"<<it->second;
+    it++;
+  }
+  
+  while(it!=m.end())
+  {
+    sout<<", "<<it->first<<":"<<it->second;
+    it++;
+  }
+  
+  sout<<" }";
+  
+  return sout.str();
+}
+
+#include <ostream>
+
+template <class K, class V>
+std::ostream& operator<<(std::ostream& os, const std::multimap<K, V>& m)
+{
+  os<<to_string(m);
+  
+  return os;
+}
+
+
+#endif
