00850c89803cddf581553dfd9b186d461574b556
myyrakle <sssang97@naver.com>
2020-06-12 06:38:57 +0900

    Create deque.h

---
+++ include/deque.h
@@ -0,0 +1,45 @@
+#ifndef __PRETTY_LOG_DEQUE__
+#define __PRETTY_LOG_DEQUE__
+
+#include <deque>
+#include <sstream>
+#include <string>
+
+template <class T>
+std::string to_string(const std::deque<T>& deq)
+{
+  std::ostringstream sout;
+  
+  sout<<"set [ ";
+  
+  auto it = deq.begin();
+  
+  if(it!=deq.end())
+  {
+    sout<<*it;;
+    it++;
+  }
+  
+  while(it!=deq.end())
+  {
+    sout<<", "<<*it;
+    it++;
+  }
+  
+  sout<<" ]";
+  
+  return sout.str();
+}
+
+#include <ostream>
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::deque<T>& s)
+{
+  os<<to_string(s);
+  
+  return os;
+}
+
+
+#endif
