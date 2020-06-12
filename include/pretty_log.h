93ce77113ff485855202c13d0a5465cd876b907a
myyrakle <sssang97@naver.com>
2020-06-12 10:45:42 +0900

    Create pretty_log.h

---
+++ include/pretty_log.h
@@ -0,0 +1,16 @@
+#ifndef __PRETTY_LOG__
+#define __PRETTY_LOG__
+
+#include "vector.h"
+#include "array.h"
+#include "raw_array.h"
+#include "list.h"
+#include "forward_list"
+#include "set.h"
+#include "unordered_set.h"
+#include "map.h"
+#include "unordered_map.h"
+#include "pair.h"
+#include "tuple.h"
+
+#endif
